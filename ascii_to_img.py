fb35548f3c0de1160b5be456782b8a7c912669bf
Mateusz Janda <mateusz.janda@gmail.com>
2019-03-23 21:17:46 +0100

    Ubuntu font, new font size

--- ascii_to_img.py
+++ ascii_to_img.py
@@ -6,8 +6,8 @@ img = Image.new('RGB', size=(400, 400))
 draw = ImageDraw.Draw(img)
 
 # font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', size=12)
-# font = ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf', size=12)
-font = ImageFont.truetype('/usr/share/fonts/truetype/ancient-scripts/Symbola_hint.ttf', size=24)
+font = ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf', size=17)
+# font = ImageFont.truetype('/usr/share/fonts/truetype/ancient-scripts/Symbola_hint.ttf', size=24)
 # font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', size=12)
 # font = ImageFont.truetype('DejaVuSansMono.ttf', size=12)
 # font = ImageFont.load('ter-u32n_unicode.pcf')
@@ -17,10 +17,11 @@ font = ImageFont.truetype('/usr/share/fonts/truetype/ancient-scripts/Symbola_hin
 
 color = 'rgb(255, 255, 255)' # black color
 
-draw.text(xy=(50, 30), text='01234567890', fill=color, font=font)
-draw.text(xy=(50, 50), text='Happy Birthday!', fill=color, font=font)
-draw.text(xy=(50, 70), text='/\\/\\/\\/\\', fill=color, font=font)
-draw.text(xy=(50, 90), text='⢧⢧⢧⢧⢧⢧⢧', fill=color, font=font)
+draw.text(xy=(50, 30), text='01234567890\nabcdefghijkl', fill=color, font=font)
+
+draw.text(xy=(50, 70), text='Happy Birthday!', fill=color, font=font)
+draw.text(xy=(50, 90), text='/\\/\\/\\/\\', fill=color, font=font)
+draw.text(xy=(50, 110), text='⢧⢧⢧⢧⢧⢧⢧', fill=color, font=font)
 
 
 img.save('output.png', 'PNG', optimize=True, quality=20)
