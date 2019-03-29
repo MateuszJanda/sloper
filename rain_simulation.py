Fixed distance calculation

rain_simulation.py
@@ -528,6 +528,7 @@ def detect_collisions(bodies, terrain):
             c = border_collision(body, terrain.size())
         collisions += c
 
+    eprint('len ', len(collisions))
     return collisions
 
 
@@ -585,7 +586,9 @@ def obstacle_collisions2(body, terrain):
 def obstacle_collisions3(body, terrain):
     result = []
     for ptpos, normal_vec in terrain.obstacles(body.ptpos, body.prev_ptpos):
-        dist = Vector(*(body.ptpos - ptpos))
+        eprint('pt to distance', body.ptpos, ptpos)
+        dist = Vector(*(ptpos - body.ptpos))
+        eprint('dist ', dist.magnitude())
         collision = Collision(body1=body,
                               body2=None,
                               relative_vel=-body.vel,
@@ -746,14 +749,21 @@ def obstacle_pos(terrain, prev_ptpos, ptpos):
 
 
 def resolve_collisions(dt, collisions):
+    mark = False
     for i in range(3):
         for c in collisions:
             # Collision with screen border
             if not c.body2:
 
-                remove = np.dot(c.relative_vel, c.normal_vec) + c.dist/dt
+                relative_vel = -c.body1.vel
+
+                # eprint(c.normal_vec)
+                eprint('relative_vel ', np.dot(relative_vel, c.normal_vec))
+                remove = np.dot(relative_vel, c.normal_vec) + c.dist/dt
+                eprint('remove', remove)
 
                 if remove < 0:
+                    mark = True
 
                     # impulse = (-(1+COEFFICIENT_OF_RESTITUTION) * np.dot(c.relative_vel, c.normal_vec)) / \
                     #         (1/c.body1.mass)
@@ -761,7 +771,7 @@ def resolve_collisions(dt, collisions):
                     impulse = (-(1+COEFFICIENT_OF_RESTITUTION) * remove) / \
                             (1/c.body1.mass)
 
-                    # eprint('pos 1', c.body1.ptpos)
+                    eprint('pos 1', c.body1.ptpos)
                     # eprint('prev 1', c.body1.prev_ptpos)
                     c.body1.vel -= (c.normal_vec / c.body1.mass) * impulse
                     c.body1.ptpos += c.body1.vel * dt
@@ -769,9 +779,12 @@ def resolve_collisions(dt, collisions):
 
                     # eprint('vel', c.body1.vel)
                     # eprint('normal', c.normal_vec)
-                    # eprint('pos 2', c.body1.ptpos)
+                    eprint('pos 2', c.body1.ptpos)
                     # time.sleep(100)
 
+
+        # if mark:
+        #     exit()
 
 def fix_penetration(bodies, terrain_size):
     for body in bodies: