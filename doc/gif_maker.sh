300cccbd610c87783370b17e24e78eabb5c02b99
Mateusz Janda <mateusz.janda@gmail.com>
2019-04-22 15:50:00 +0200

    Added gif maker

---
+++ doc/gif_maker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VIDEO_FILE="caputerd_screen.mp4"
+
+if [[ $1 == "-c" || $1 == "--capture" ]]; then
+    ffmpeg -video_size 640x480 -framerate 25 -f x11grab -i :0.0+0,48 $VIDEO_FILE
+elif [[ $1 == "-g" || $1 == "--video-to-gif" ]]; then
+    PALETTE_FILE="tmp_pallete.png"
+    OUTPUT_GIF="output.gif"
+    FILTERS="fps=25"
+
+    ffmpeg -v warning -i $VIDEO_FILE -vf "$FILTERS,palettegen" -y $PALETTE_FILE
+    ffmpeg -v warning -i $VIDEO_FILE -i $PALETTE_FILE -lavfi "$FILTERS [x]; [x][1:v] paletteuse" -y $OUTPUT_GIF
+else
+    echo "Warning. No parameters."
+    echo "Usage:"
+    echo "   ./gif_maker.sh -c"
+    echo "   ./gif_maker.sh -g"
+fi
+
